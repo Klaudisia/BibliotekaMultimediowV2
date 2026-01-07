cdd496e315094faf4dfd51b2c3fb112867b1fbe1
Klaudia <kokoszanekklaudia0806@gmail.com>
2026-01-07 21:56:53 +0100

    dodana funkcja wyszukiwania

--- obj/Debug/net9.0/BibliotekaMultimediowV2.AssemblyInfo.cs
+++ obj/Debug/net9.0/BibliotekaMultimediowV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotekaMultimediowV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8ffd349666b2ae9c586a48833a3d03b45b950c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836ba45115efbe65ee2c5c57812614069cc16ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotekaMultimediowV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotekaMultimediowV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
